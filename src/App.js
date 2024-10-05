d8b67493b94987b3387e9910112260460609a207
Khoi Vu <46497248+khoiCodeDump@users.noreply.github.com>
2024-10-05 12:51:44 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+const name = "khoi";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>My name is {name} </h2>
+    </div>
+  );
+}
